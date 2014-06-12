ed0456831a7a902be0cdf7f3fc5979e55509e4df
ronald <ronald@ronaldubuntu.(none)>
2014-06-12 11:09:48 +0700

    constans.php

--- application/config/constants.php
+++ application/config/constants.php
@@ -39,7 +39,7 @@ define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 /*
     VARIABEL TAMBAHAN
 */
-
+/*
 define('TITLEAPP', 'BADIKLAT');
 define('BREADCRUMBDEF', '');
 define('FILEFOLDER', 'folderupload');
@@ -47,10 +47,25 @@ define('UPLOADPATH', 'D:\\xampp\\htdocs\\badiklatci\\folderupload\\');
 define('LIMITPAGING', 20);
 define('BASEURL', 'http://localhost:8080/badiklatci/');
 
-/*SETTING DB START*/
+// SETTING DB START
 define('HOSTDB','localhost');
 define('USERDB','root');
 define('PASSDB','gakpake');
+define('NAMEDB','badiklat');
+*/
+
+
+define('TITLEAPP', 'BADIKLAT');
+define('BREADCRUMBDEF', '');
+define('FILEFOLDER', 'folderupload');
+define('UPLOADPATH', '/home/ronald/public_html/badiklatci/folderupload');
+define('LIMITPAGING', 20);
+define('BASEURL', 'http://localhost/badiklatci/');
+
+// SETTING DB START
+define('HOSTDB','localhost');
+define('USERDB','root');
+define('PASSDB','rahasia');
 define('NAMEDB','badiklat');
 /*SETTING DB END*/
 
